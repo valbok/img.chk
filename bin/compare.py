73aae0d68fcb77bdfbae8dcf959b7948400ca20d
VaL <vd@nxc.no>
2013-12-02 22:57:03 +0100

    - Removed plot

--- bin/compare.py
+++ bin/compare.py
@@ -12,7 +12,6 @@ import sys
 import os
 parentdir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 os.sys.path.insert( 0, parentdir )
-from matplotlib import pyplot as plt
 
 import json
 from core import *
