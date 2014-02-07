1cedad1654e57d6e00c707119956920ab501c2ef
VaL <vd@nxc.no>
2014-02-07 20:39:36 +0200

    - Removed unneeded tests

--- tests/hash/extractor.py
+++ tests/hash/extractor.py
@@ -210,51 +210,6 @@ class ExtractorTest( unittest.TestCase ):
         self.assertEquals( e, len( p ) > 4 )
         #self.assertEquals( e, len( a ) > 4 )
 
-    def testRanBinHashes( self ):
-        from matplotlib import pyplot as plt
-        img1 = Image.read( "tests/images/lenna_full.jpg" )
-        w = img1.width
-        h = img1.height
-        m1 = ( img1.width + img1.height ) / 2
-        kp1,desc1 = cv2.ORB(m1).detectAndCompute( img1.img, None )
-        i = 0
-        while i < 5:
-            print i
-            x = random.randrange( 0, w )
-            y = random.randrange( 0, h )
-            dx = random.randrange( 0, w/2 )
-            dy = random.randrange( 0, h/2 )
-            x1 = x - dx
-            y1 = y - dy
-            x2 = x + dx
-            y2 = y + dy
-
-            img2 = img1.crop( x1, x2, y1, y2 )
-            if not img2:
-                continue
-
-            #plt.imshow(img2.img),plt.show()
-
-            m2 = ( img2.width + img2.height ) / 2
-
-            kp2,desc2 = cv2.ORB(m2).detectAndCompute( img2.img, None )
-            if desc2 is None:
-                continue
-
-            e1 = Extractor( img1, kp1, desc1 )
-            e2 = Extractor( img2, kp2, desc2 )
-
-            imgs1 = e1.binImages()
-            imgs1.append( img1 )
-            imgs2 = e2.binImages()
-            imgs2.append( img2 )
-            d = 8
-            matcher = Matcher()
-            matches = matcher.match( imgs1, imgs2, d )
-            p = [m.type for m in matches if m.type == "PHash"]
-
-            self.assertEquals( True, len( p ) > 4 )
-            i += 1
 
 if __name__ == '__main__':
     unittest.main()
--- tests/image.py
+++ tests/image.py
@@ -16,37 +16,37 @@ class ImageTest( unittest.TestCase ):
             Image.read( "wrong/path.jpg" )
 
     def testCorretFN( self ):
-        img = Image.read( "tests/core/images/1.jpg" )
+        img = Image.read( "tests/images/1.jpg" )
         self.assertEquals( True, isinstance( img, Image ) )
 
     def testResize( self ):
-        img = Image.read( "tests/core/images/5.jpg" )
+        img = Image.read( "tests/images/5.jpg" )
         img2 = img.resize( (32, 32) )
         self.assertEquals( 32, img2.width )
         self.assertEquals( 32, img2.height )
 
     def testShape( self ):
-        img = Image.read( "tests/core/images/5.jpg" )
+        img = Image.read( "tests/images/5.jpg" )
         s = img.shape
         self.assertEquals( 399, s[0] )
         self.assertEquals( 500, s[1] )
 
     def testWidth( self ):
-        img = Image.read( "tests/core/images/5.jpg" )
+        img = Image.read( "tests/images/5.jpg" )
         s = img.width
         self.assertEquals( 500, s )
 
     def testHeight( self ):
-        img = Image.read( "tests/core/images/5.jpg" )
+        img = Image.read( "tests/images/5.jpg" )
         s = img.height
         self.assertEquals( 399, s )
 
     def testPixel( self ):
-        img = Image.read( "tests/core/images/1.jpg", 0 )
+        img = Image.read( "tests/images/1.jpg", 0 )
         self.assertEquals( 4, img.pixel( 0, 0 ) )
 
     def testCrop( self ):
-        img = Image.read( "tests/core/images/3.jpg", 0 )
+        img = Image.read( "tests/images/3.jpg", 0 )
         img2 = img.crop( 0, 0, 20, 10 )
         self.assertEquals( 20, img2.width )
         self.assertEquals( 10, img2.height )
